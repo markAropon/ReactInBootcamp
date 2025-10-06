87656f8083bf2e3ab8a05f534c0427ab26235063
MarkAropon <aropon.markjames.august17@gmail.com>
2025-10-06 11:08:03 +0800

    quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { AnimatePresence } from "framer-motion";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
+import HorizontalPageAnimation from "./common/HorizontalPageAnimation";
 import PageAnimation from "./common/PageAnimation";
 import Home from "./pages/Home";
-// import ToDo from "./pages/ToDO"; TODO: Uncomment Before Pushing
-import HorizontalPageAnimation from "./common/HorizontalPageAnimation";
 import Page1 from "./pages/RouterSample/Page1";
 import Page2 from "./pages/RouterSample/Page2";
 import Page3 from "./pages/RouterSample/Page3";
 import Page4 from "./pages/RouterSample/Page4";
 import Page5 from "./pages/RouterSample/Page5";
-import ToDo from "./pages/ToDo";
+import ToDo from "./pages/ToDO";
+// import ToDo from "./pages/ToDo";
 import TypingTest from "./pages/TypingTest";
 
 const AnimatedRoutes = () => {
